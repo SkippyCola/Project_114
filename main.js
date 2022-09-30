4bbda41b44b3c2d7f285a4491dd905dc9c209b66
SkippyCola <113696814+SkippyCola@users.noreply.github.com>
2022-09-30 21:30:13 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function preload()
+{
+
+}
+
+function draw()
+{
+
+}
+
+function setup()
+{
+    canvas = createCanvas(300,300);
+    canvas.center();
+}
+
+function take_snapshot()
+{
+    save('RealtimeFilterPhoto.png');
+}
